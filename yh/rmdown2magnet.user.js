7eb8e1441f8834483aa4cbafb45ced77ebe1c082
Evan <evan7fan@outlook.com>
2022-01-29 17:48:36 +0800

    Update rmdown2magnet.user.js

--- yh/rmdown2magnet.user.js
+++ yh/rmdown2magnet.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         rmdown2magnet 草榴转换磁力链
 // @namespace    https://ef6.github.io
-// @version      0.2
+// @version      0.3
 // @description  Automatically convert rmdown to magnet
 // @author       ef6
 // @include      http*://t66y.com/htm_data/*.html
@@ -12,14 +12,14 @@
 (function() {
 'use strict';
 
-let tx = document.getElementsByTagName("a");
-for(let i=0;i<tx.length;i++){
-	if(tx[i].text.search("hash=")>0){
-		let magnet = 'magnet:?xt=urn:btih:' + tx[i].href.split('=')[1].slice(3);
+let nd = document.querySelectorAll(".tpc_cont>a");
+for(let i=0;i<nd.length;i++){
+	if(nd[i].text.search("hash=")>0){
+		let magnet = 'magnet:?xt=urn:btih:' + nd[i].href.split('=')[1].slice(3);
 		console.log(magnet);
-        tx[i].innerText=magnet;
-		tx[i].href=magnet;
-        tx[i].className="h f18"
+		nd[i].innerText=magnet;
+		nd[i].href=magnet;
+		nd[i].className="h f18"
 	}
 }
 
