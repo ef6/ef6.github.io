b208de7307f8c7d2d07fbc78cb2a2a6864e52aa5
Evan <evan7fan@outlook.com>
2022-01-29 16:49:20 +0800

    Add files via upload

---
+++ yh/rmdown2magnet.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         rmdown2magnet 草榴转换磁力链
+// @namespace    https://ef6.github.io
+// @version      0.1
+// @description  Automatically convert rmdown to magnet
+// @author       ef6
+// @include      http*://t66y.com/htm_data/*.html
+// @include      http*://t66y.com/htm_mob/*.html
+// @grant        none
+// ==/UserScript==
+
+(function() {
+'use strict';
+
+let tx = document.getElementsByTagName("a");
+for(let i=0;i<tx.length;i++){
+	if(tx[i].text.search("hash=")>0){
+		let magnet = 'magnet:?xt=urn:btih:' + tx[i].href.split('=')[1].slice(3);
+		console.log(magnet);
+        tx[i].innerText=magnet;
+		tx[i].href=magnet;
+        tx[i].className="t_like"
+	}
+}
+
+})();
