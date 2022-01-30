c877439226e8ce30fcef6a0adb2a2b9b6fccb6a8
Evan <evan7fan@outlook.com>
2022-01-30 14:38:30 +0800

    Update jy.zrzyj.jiaozuo.user.js

--- yh/jy.zrzyj.jiaozuo.user.js
+++ yh/jy.zrzyj.jiaozuo.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         焦作市自然资源网上交易系统显示单价
 // @namespace    https://ef6.github.io
-// @version      0.1
+// @version      0.2-20220130
 // @description  焦作市自然资源网上交易系统显示单价
 // @author       ef6
 // @match        http*://jy.zrzyj.jiaozuo.gov.cn*/trade-engine/trade/resources*
@@ -17,12 +17,14 @@
             for(let n of rlst){
                 let mj=n.querySelector("span[ng-bind*=CRMJ]").innerText;
                 let jg=n.querySelector("span[ng-bind*=ZGBJ]").innerText.slice(1);
-                let dj=(jg*10000)/mj;
-                let djm=dj/15;
-                let el = document.createElement("a");
-                el.className="dj-tag color-red font-bold";
-                el.innerText=String.format("{0}元/㎡ {1}万元/亩",dj.toFixed(0),djm.toFixed(0));
-                n.querySelector(".resource-list-header").append(el)
+                if(mj>0 && jg>0){
+                    let dj=(jg*10000)/mj;
+                    let djm=dj/15;
+                    let el = document.createElement("a");
+                    el.className="dj-tag color-red font-bold";
+                    el.innerText=String.format("{0}元/㎡ {1}万元/亩",dj.toFixed(0),djm.toFixed(0));
+                    n.querySelector(".resource-list-header").append(el)
+                }
             }
         }
     },1500);
